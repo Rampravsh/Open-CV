5943aee43e2d3ddba5e94a357ce5bfa15be9b5aa
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-31 00:23:10 +0530

    draw circle on image

---
+++ image drawing function/draw_circle.py
@@ -0,0 +1,12 @@
+import cv2
+image =cv2.imread("input.jpg")
+if image is not None:
+    print("image loaded successfully")
+    center=(500,500)
+    radius=500
+    color=(255,0,0)
+    thickness=4
+    cv2.circle(image,center,radius,color,thickness)
+    cv2.imshow("line Drawing", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
