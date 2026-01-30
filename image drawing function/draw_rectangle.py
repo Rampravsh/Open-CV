a81b8b864f2665c32ccb93c58da31a78722a3e03
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-31 00:17:53 +0530

    draw rectangle on image

---
+++ image drawing function/draw_rectangle.py
@@ -0,0 +1,12 @@
+import cv2
+image =cv2.imread("input.jpg")
+if image is not None:
+    print("image loaded successfully")
+    pt1=(50,100)
+    pt2=(300,200)
+    color=(255,0,0)
+    thickness=4
+    cv2.rectangle(image,pt1,pt2,color,thickness)
+    cv2.imshow("line Drawing", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
